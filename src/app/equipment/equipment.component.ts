e2c8803b58a56d18d1196c0dbdb9fff470f9aca2
Lisa Bartfeld <LisaBartfeld@gmail.com>
2020-12-10 22:03:20 -0600

    Equipment bonus mission complete

--- src/app/equipment/equipment.component.ts
+++ src/app/equipment/equipment.component.ts
@@ -7,9 +7,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EquipmentComponent implements OnInit {
 
+  equipment: object[] = [
+    {name: "Habitat dome"},
+    {name: "Drones"},
+    {name: "Food containers"},
+    {name: "oxygen tanks"}
+  ];
+
+    memberBeingEdited: object = null;
+
   constructor() { }
 
   ngOnInit() {
   }
 
+  add(memberName: string) {
+    this.equipment.push({name: memberName});
+  }
+    
+  remove(member: object) {
+    let index = this.equipment.indexOf(member);
+    this.equipment.splice(index, 1);
+  }
+
+  edit(member: object) {
+    this.memberBeingEdited = member;
+  }
+
+  save(name: string, member: object) {
+    member['name'] = name;
+    this.memberBeingEdited = null;
+}
 }
